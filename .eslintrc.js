e2cc471356e6cf8626a30ab088da13540498c646
Chris Vaughan <vaughan99@yahoo.com>
2026-01-16 15:19:29 -0600

    prettier in vscode works

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,77 +1,92 @@
 module.exports = {
-settings: {
-'import/resolver': {
-node: {
-extensions: ['.js', '.jsx', '.ts', '.tsx'],
-},
-},
-},
-env: {
-es6: true,
-node: true,
-jest: true,
-},
-extends: [
-'airbnb-base',
-// See https://www.npmjs.com/package/eslint-plugin-prettier/
-'plugin:prettier/recommended',
-],
-globals: {
-Atomics: 'readonly',
-BigInt: true,
-SharedArrayBuffer: 'readonly',
-},
-parser: '@typescript-eslint/parser',
-parserOptions: {
-ecmaVersion: 2018,
-sourceType: 'module',
-},
-plugins: ['@typescript-eslint', 'jest', 'prettier'],
-rules: {
-'no-console': 'off',
-'no-shadow': 'off',
-'import/prefer-default-export': 'off',
-'import/extensions': [
-'error',
-'never',
-{
-js: 'never',
-mjs: 'never',
-jsx: 'never',
-},
-],
-'no-unused-vars': 'off', // covered by @typescript-eslint/no-unused-vars
-'import/no-extraneous-dependencies': ['error', { packageDir: ['./'] }],
-'prettier/prettier': 'error',
-'@typescript-eslint/explicit-function-return-type': [
-'error',
-{ allowExpressions: true },
-],
-'@typescript-eslint/no-explicit-any': ['error'],
-'@typescript-eslint/no-unused-vars': [
-'error',
-{
-vars: 'all',
-args: 'after-used',
-ignoreRestSiblings: true,
-argsIgnorePattern: '^_',
-},
-],
-'max-len': ['error', { code: 120 }],
-'lines-between-class-members': [
-'error',
-'always',
-{ exceptAfterSingleLine: true },
-],
-'object-curly-newline': [
-'error',
-{
-ImportDeclaration: {
-minProperties: 6,
-consistent: false,
-multiline: true,
-},
-},
-],
-},
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.svelte', '.css'],
+      },
+    },
+  },
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    // See https://www.npmjs.com/package/eslint-plugin-prettier/
+    'plugin:prettier/recommended',
+    'plugin:svelte/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    BigInt: true,
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest', 'prettier', 'svelte'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+      },
+    ],
+    'no-unused-vars': 'off', // covered by @typescript-eslint/no-unused-vars
+    'import/no-extraneous-dependencies': ['error', { packageDir: ['./'] }],
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true },
+    ],
+    '@typescript-eslint/no-explicit-any': ['error'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'max-len': ['error', { code: 120 }],
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ImportDeclaration: {
+          minProperties: 6,
+          consistent: false,
+          multiline: true,
+        },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      // The parser options will apply to the content inside <script> and <template> tags
+      parserOptions: {
+        parser: '@typescript-eslint/parser', // Use TypeScript parser if you have TypeScript
+        // Add extra file extensions for TypeScript to function correctly
+        extraFileExtensions: ['.svelte'],
+        // If using a specific tsconfig.json, uncomment and set the project path:
+        // project: 'path/to/your/tsconfig.json'
+      },
+    },
+  ],
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -3,5 +3,6 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  plugins: ['./node_modules/prettier-plugin-jsdoc/dist/index.js'],
+  plugins: ['prettier-plugin-jsdoc', 'prettier-plugin-svelte'/* , 'prettier-plugin-tailwindcss' */],
+  overrides:  [{ "files": "*.svelte", "options": { "parser": "svelte" } }]
 };
