ff22aa4b45eff42900e132bb83055e941a21b485
Chris Vaughan <vaughan99@yahoo.com>
2026-01-18 13:38:07 -0600

    start on dice bag

---
+++ src/lib/components/gmgen/DiceBag.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { DiceRoll } from '@dice-roller/rpg-dice-roller';
+    import { memoize } from 'lodash';
+    import { Button } from "$lib/components/ui/button";
+    import * as Resizable from "$lib/components/ui/resizable/index.js";
+
+    const diceGenerator = memoize((expr: string) => new DiceRoll(expr));
+
+    const rollDice = (expr: string) => () => {
+        return diceGenerator(expr).roll();
+    }
+</script>
+
+<Resizable.PaneGroup direction="horizontal" class="max-w-md rounded-lg border">
+  <Resizable.Pane defaultSize={50}>
+    <div class="flex h-[200px] items-center justify-center p-6">
+        <Button>d2</Button>
+        <Button>d3</Button>
+        <Button>d4</Button>
+        <Button>d6</Button>
+        <Button>d8</Button>
+        <Button>d10</Button>
+        <Button>d12</Button>
+        <Button>d20</Button>
+        <Button>d100</Button>
+    </div>
+  </Resizable.Pane>
+  <Resizable.Handle />
+  <Resizable.Pane defaultSize={50}>
+    <Resizable.PaneGroup direction="vertical">
+      <Resizable.Pane defaultSize={25}>
+        <div class="flex h-full items-center justify-center p-6">
+          <span class="font-semibold">Two</span>
+        </div>
+      </Resizable.Pane>
+      <Resizable.Handle />
+      <Resizable.Pane defaultSize={75}>
+        <div class="flex h-full items-center justify-center p-6">
+          <span class="font-semibold">Three</span>
+        </div>
+      </Resizable.Pane>
+    </Resizable.PaneGroup>
+  </Resizable.Pane>
+</Resizable.PaneGroup>
--- src/lib/components/gmgen/GMGen.svelte
+++ src/lib/components/gmgen/GMGen.svelte
@@ -2,6 +2,8 @@
   // import './app.css';
   // import type { MouseEventHandler } from 'svelte/elements';
   import Menu from '$lib/components/gmgen/Menu.svelte';
+  import * as Tabs from "$lib/components/ui/tabs/index.js";
+  import DiceBag from '$lib/components/gmgen/DiceBag.svelte';
 
   /*
   const openExternal: MouseEventHandler<HTMLAnchorElement> = (e) => {
@@ -14,5 +16,36 @@
 <main>
   <Menu />
   <h1>GMGen</h1>
-
+ 
+  <div class="-mb-4 flex w-full max-w-sm flex-col gap-6">
+    <Tabs.Root value="account">
+      <Tabs.List>
+        <Tabs.Trigger value="diceBag">Dice Bag</Tabs.Trigger>
+        <Tabs.Trigger value="encounter">Encounter</Tabs.Trigger>
+        <Tabs.Trigger value="initiative">Initiative</Tabs.Trigger>
+        <Tabs.Trigger value="experienceAdjuster">Experience Adjuster</Tabs.Trigger>
+        <Tabs.Trigger value="notes">Notes</Tabs.Trigger>
+        <Tabs.Trigger value="randomName">Random Name</Tabs.Trigger>
+        <Tabs.Trigger value="overlandTravel">Overland Travel</Tabs.Trigger>
+        <Tabs.Trigger value="characterTracker">Character Tracker</Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="diceBag">
+        <DiceBag />
+      </Tabs.Content>
+      <Tabs.Content value="encounter">
+      </Tabs.Content>
+      <Tabs.Content value="initiative">
+      </Tabs.Content>
+      <Tabs.Content value="experienceAdjuster">
+      </Tabs.Content>
+      <Tabs.Content value="notes">
+      </Tabs.Content>
+      <Tabs.Content value="randomName">
+      </Tabs.Content>
+      <Tabs.Content value="overlandTravel">
+      </Tabs.Content>
+      <Tabs.Content value="characterTracker">
+      </Tabs.Content>
+    </Tabs.Root>
+  </div>
 </main>
