b3819be5d4762c351a5bfc3e89a275aa6e5e0192
Chris Vaughan <vaughan99@yahoo.com>
2026-01-23 17:36:35 -0600

    use 2xs and 3xs suffix

--- src/lib/components/ui/menubar/menubar-shortcut.svelte
+++ src/lib/components/ui/menubar/menubar-shortcut.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import { cn, type WithElementRef } from "$lib/utils.js";
+  import type { HTMLAttributes } from 'svelte/elements';
+  import { cn, type WithElementRef } from '$lib/utils.js';
 
-	let {
-		ref = $bindable(null),
-		class: className,
-		children,
-		...restProps
-	}: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
 </script>
 
 <span
-	bind:this={ref}
-	data-slot="menubar-shortcut"
-	class={cn("text-muted-foreground ms-auto text-xxs tracking-widest", className)}
-	{...restProps}
+  bind:this={ref}
+  data-slot="menubar-shortcut"
+  class={cn(
+    'text-muted-foreground ms-auto text-2xs tracking-widest',
+    className,
+  )}
+  {...restProps}
 >
-	{@render children?.()}
+  {@render children?.()}
 </span>
